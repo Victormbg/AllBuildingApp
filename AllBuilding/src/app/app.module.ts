5a239992a1cf9037be6fd1fa8cdfd611d7958398
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2019-11-04 23:38:50 +0000

    update firebase

--- AllBuilding/src/app/app.module.ts
+++ AllBuilding/src/app/app.module.ts
@@ -9,15 +9,25 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//importações DB//
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    entryComponents: [],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFirestoreModule,
+        AngularFireDatabaseModule,
+        AngularFireAuthModule,],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- AllBuilding/src/environments/environment.ts
+++ AllBuilding/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBC_qevvhRYXUl1K4lUngdDvXbn_wjxN64",
+    authDomain: "allbuilding2.firebaseapp.com",
+    databaseURL: "https://allbuilding2.firebaseio.com",
+    projectId: "allbuilding2",
+    storageBucket: "allbuilding2.appspot.com",
+    messagingSenderId: "562656809576",
+    appId: "1:562656809576:web:3761935e546023a6eba1dd"
+  }
 };
 
 /*
