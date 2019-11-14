04a8b63d044052a8fc8b1d5901c840c0067d2d9c
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2019-11-14 00:21:02 +0000

    UPDATES VICTOR AS 21:20H

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,6 @@ const routes: Routes = [
     path: "",
     loadChildren: () => import("./tabs/tabs.module").then(m => m.TabsPageModule)
   },
-  { path: "", redirectTo: "home", pathMatch: "full" },
-  {
-    path: "home",
-    loadChildren: () =>
-      import("./pages/home/home.module").then(m => m.HomePageModule)
-  },
   {
     path: "tab4",
     loadChildren: "../app/pages/destaque/tab4.module#Tab4PageModule",
