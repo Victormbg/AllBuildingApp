c957837c04afc8ca6a1088dc718b3b8e80b4426f
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2019-12-11 13:45:42 +0000

    UPDATE VICTOR AS 10:45H

--- src/app/redefiir.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RedefiirPipe } from './redefiir.pipe';
-
-describe('RedefiirPipe', () => {
-  it('create an instance', () => {
-    const pipe = new RedefiirPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
